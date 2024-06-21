990ecd5aade7f4c74fb39223ed41b452f2079ba9
Diegodfp <diegodfp.fly@gmail.com>
2024-06-21 16:53:21 -0500

    comienzo

---
+++ betplay/src/main/java/com/betplay/Controller.java
@@ -0,0 +1,10 @@
+package com.betplay;
+
+
+import java.util.Hashtable;
+
+import com.betplay.model.entity.Team;
+
+public class Controller {
+    public Hashtable <String,Team> equipos = new Hashtable<>();
+}
---
+++ betplay/src/main/java/com/betplay/view/viewPlayer.java
@@ -0,0 +1,8 @@
+package com.betplay.view;
+
+import com.betplay.model.entity.Player;
+
+public class viewPlayer {
+       Player jugador = new Player();
+
+}
