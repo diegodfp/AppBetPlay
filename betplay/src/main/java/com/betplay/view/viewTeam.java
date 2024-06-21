fc8936d815eed88c25e5af69dacb34152c7eb972
Diegodfp <diegodfp.fly@gmail.com>
2024-06-21 17:42:31 -0500

    actualizaryEliminar

--- betplay/src/main/java/com/betplay/view/viewTeam.java
+++ betplay/src/main/java/com/betplay/view/viewTeam.java
@@ -7,6 +7,7 @@ import com.betplay.model.entity.Team;
 
 public class viewTeam {
     public static Controller controlador;
+
     public void start() {
         Scanner scanner = new Scanner(System.in);
 
@@ -17,31 +18,54 @@ public class viewTeam {
             System.out.println("4. Eliminar Equipo");
             System.out.println("5. Listar todos Equipos");
             System.out.println("6. Salir");
+            String  codBusqueda = "";
             int choice = scanner.nextInt();
             scanner.nextLine(); // Consume newline
-
+            Team equipo = new Team();
             switch (choice) {
                 case 1:
-                        Team equipo = new Team();
-                        String codigoEquipo = null;
-                        System.out.println("Ingrese el codigo del equipo :");
-                        codigoEquipo = scanner.nextLine();
-                        System.out.println("Ingrese Nombre del equipo :");
-                        equipo.setNombre(scanner.nextLine());
-                        System.out.println("Ingrese la ciudad :");
-                        equipo.setCiudad(scanner.nextLine());
-                        controlador.equipos.put(codigoEquipo, equipo);
+
+                    String codigoEquipo = null;
+                    System.out.println("Ingrese el codigo del equipo :");
+                    codigoEquipo = scanner.nextLine();
+                    System.out.println("Ingrese Nombre del equipo :");
+                    equipo.setNombre(scanner.nextLine());
+                    System.out.println("Ingrese la ciudad :");
+                    equipo.setCiudad(scanner.nextLine());
+                    controlador.equipos.put(codigoEquipo, equipo);
+                    System.out.println("Equipo creado correctamente");
+                    System.out.println("presione cualquier tecla para continuar");
+                    scanner.nextLine();
+
                     break;
 
                 case 2:
-
+                    System.out.println("ingrese el codigo del equipo que desea actualizar");
+                    codBusqueda = scanner.nextLine();
+                    if (controlador.equipos.containsKey(codBusqueda)) {
+                        Team equipoActualizado = new Team();
+                        System.out.println("Ingrese el nuevo nombre del equipo");
+                        String newNombre = scanner.nextLine();
+                        System.out.println("Ingrese la nueva ciudad del equipo");
+                        String newCiudad = scanner.nextLine();
+                        equipoActualizado.setCiudad(newCiudad);
+                        equipoActualizado.setNombre(newNombre);
+                        controlador.equipos.put(codBusqueda, equipoActualizado);
+                        System.out.println("Equipo actualizado correctamente");
+                        System.out.println("presione cualquier tecla para continuar");
+                        scanner.nextLine();
+                    } else {
+                        System.out.println("codigo de equipo no encontrado intente nuevamente");
+                        continue;
+                    }
                     break;
 
                 case 3:
+                    System.out.println("ingrese el codigo del equipo que desea buscar");
+                    codBusqueda = scanner.nextLine();
                     Team eq = new Team();
-                    String codigoE = "001";
-                    eq = controlador.equipos.get(codigoE);
-                    System.out.println("Mi equipo" + eq.getNombre());
+                    eq = controlador.equipos.get(codBusqueda);
+                    System.out.println("El equipo con codigo " + codBusqueda + " es el equipo " + eq.getNombre() + " de la ciudad de " + eq.getCiudad());
                     break;
                 case 4:
 
